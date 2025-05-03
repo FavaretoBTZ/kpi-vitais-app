bb7fd2aba0fcacbf8eb9f2e7a1d6c3436a45fb9b
FavaretoBTZ <vitor@btzmotorsport.com>
2025-05-03 13:02:53 -0300

    Update kpi_vitais_app_com_logo.py

--- kpi_vitais_app_com_logo.py
+++ kpi_vitais_app_com_logo.py
@@ -34,12 +34,16 @@ if uploaded_file is not None:
  # --- Sidebar para seleção ---
     st.sidebar.header("Filtros")
     car_alias = st.sidebar.selectbox("Selecione o CarAlias:", df['CarAlias - Info'].unique())
+    track_options = ["Visualizar Todos"] + sorted(df[col_track].dropna().unique().tolist())
+    track_selected = st.sidebar.selectbox("Selecione a Etapa (TrackName):", track_options)
     y_axis = st.sidebar.selectbox("Selecione a métrica (Y Axis) para o gráfico 1:", list(df.columns[8:41]), key="metric_1")
     y_axis_2 = st.sidebar.selectbox("Selecione a métrica (Y Axis) para o gráfico 2:", list(df.columns[8:41]), key="metric_2")
     y_axis_scatter = st.sidebar.selectbox("Selecione a métrica para Scatter Plot:", list(df.columns[8:41]), key="scatter_metric")
 
     filtered_df = df[df['CarAlias - Info'] == car_alias]
-    filtered_df = filtered_df.sort_values(by=['SessionDate - Info', 'SessionName - Info', 'Lap - Info'])
+if track_selected != "Visualizar Todos":
+    filtered_df = filtered_df[filtered_df['TrackName - Info'] == track_selected]
+filtered_df = filtered_df.sort_values(by=['SessionDate - Info', 'SessionName - Info', 'Lap - Info'])
 
     # --- Gráfico 1 ---
     fig = px.line(
