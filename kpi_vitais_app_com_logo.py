1db4b28a35633393ead0cb724b567fb9435cd846
FavaretoBTZ <vitor@btzmotorsport.com>
2025-06-02 22:48:21 -0300

    Update kpi_vitais_app_com_logo.py

--- kpi_vitais_app_com_logo.py
+++ kpi_vitais_app_com_logo.py
@@ -187,6 +187,7 @@ if uploaded_file is not None:
         font=dict(size=10)
     )
 )
+    st.subheader("Gráfico 3 - Dispersão Personalizada")
     st.plotly_chart(fig3, use_container_width=True)
 
     # --- Exportar gráficos para PDF ---
